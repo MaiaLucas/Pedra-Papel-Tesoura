258567e50b711a17a7f4db90de4f7eea355bd1f0
MaiaLucas <barretomaia5466@outlook.com>
2019-02-13 23:28:37 -0300

    adding functions restartGame and saveGame

--- jogo/js/app.js
+++ jogo/js/app.js
@@ -1,17 +1,20 @@
 let userScore  = 0;
 let compScore  = 0;
-let score_table = document.getElementsByClassName("scoreTable");
+let round      = 1;
+
+
+const score_table      = document.getElementsByClassName("scoreTable");
 
 const userScore_span = document.getElementById("user-score");
 const compScore_span = document.getElementById("comp-score");
-
-const scoreBoard_div = document.querySelector(".score-board");
-const result_p       = document.querySelector(".result > p");
-
 const rock_div       = document.getElementById("pedra");
 const paper_div      = document.getElementById("papel");
 const scissors_div   = document.getElementById("tesoura");
 const restart_button = document.getElementById("restart");
+
+const scoreBoard_div = document.querySelector(".score-board");
+const result_p       = document.querySelector(".result > p");
+
 //--
 
 function getComputerChoice() {
@@ -20,56 +23,75 @@ function getComputerChoice() {
     return choices[randomNumber];
 }
 
+//-------
 const smallUserWord = "user".fontsize(3).sub();
 const smallCompWord = "comp".fontsize(3).sub();
 function win(user, computer) {
     const userScore_div = document.getElementById(user);
     userScore ++;
+
     userScore_span.innerHTML = userScore;
     compScore_span.innerHTML = compScore;
-    result_p.innerHTML = `${user}${smallUserWord} ganha de ${computer}${smallCompWord}. Você ganhou!`;
+
+    result_p.innerHTML = `${user}${smallUserWord} win to ${computer}${smallCompWord}. YOU WIN!`;
     userScore_div.classList.add('green-glow');
+    
     setTimeout( () => userScore_div.classList.remove('green-glow'), 500);
+
     if( userScore == 7 ) {
-        //console.log( "Jogador Ganhou a rodada!" )
-        alert(`Jogador ganhou! Placar: ${userScore_span.innerHTML} x ${compScore_span.innerHTML}`);
+        prompt(`User Wins! Score: ${userScore_span.innerHTML} x ${compScore_span.innerHTML}`);
         getScoreTable(userScore_span.innerHTML,compScore_span.innerHTML);
-        restart();
+
+        document.onchange = saveGame();
     }
+
 }
 
 function lose(user, computer) {
     const userScore_div = document.getElementById(user);
     compScore ++;
+
     userScore_span.innerHTML = userScore;
     compScore_span.innerHTML = compScore;
-    result_p.innerHTML = `${user}${smallUserWord} perde para ${computer}${smallCompWord}. Você perdeu!`;
+
+    result_p.innerHTML = `${user}${smallUserWord} lose for ${computer}${smallCompWord}. YOU LOSE!`;
     userScore_div.classList.add('red-glow');
+
     setTimeout( () => userScore_div.classList.remove('red-glow'), 500);
+
     if( compScore == 7 ) {
-        //console.log( "Jogador Ganhou a rodada!" )
-        alert(`Computador ganhou! Placar: ${userScore_span.innerHTML} x ${compScore_span.innerHTML}`);
+        prompt(`Computer Wins! Score: ${userScore_span.innerHTML} x ${compScore_span.innerHTML}`);
         getScoreTable(userScore_span.innerHTML,compScore_span.innerHTML);
-        restart();
+
+        document.onchange = saveGame();
     }
+
 }
 
 function draw(user, computer) {
     const userScore_div = document.getElementById(user);
-    result_p.innerHTML = `${user}${smallUserWord} empata com ${computer}${smallCompWord}. Empatou!`;
+
+    result_p.innerHTML = `${user}${smallUserWord} draw with ${computer}${smallCompWord}. DRAW!`;
     userScore_div.classList.add('gray-glow');
+
     setTimeout( () => userScore_div.classList.remove('gray-glow'), 500);
 }
+//-------
 
-function restart () {
-    userScore_span.innerHTML = 0;
-    compScore_span.innerHTML = 0;
+function restartGame() {
+    history.go(0);
 }
 
 function getScoreTable (user, computer) {
     let $score_table = $('.scoreTable');
-    $score_table.prepend( `<tr> <td>${user}</td><td>${computer}</td> </tr>` );
-    console.log(`${user}  ${computer}`);
+
+    document.onchange = saveGame();
+    user = localStorage.getItem(userScore_span);
+    compputer = localStorage.getItem(compScore_span);
+    console.log(user + " x " + computer);
+
+    $score_table.append( `<tr> <td>${round}</td><td>${user}</td><td>${computer}</td> </tr>` );
+    round++;
 }
 
 function game(userChoice) {
@@ -97,11 +119,20 @@ function game(userChoice) {
     }
 }
 
+function saveGame() {
+    let compScore_storage = compScore_span.innerHTML;
+    let userScore_storage = userScore_span.innerHTML;
+
+    localStorage.setItem('user-score', userScore_storage);
+    localStorage.setItem('comp-score', compScore_storage);
+}
+
+
 function main() {
     rock_div.addEventListener( 'click', () => game("pedra") );
     paper_div.addEventListener( 'click', () => game("papel") );
     scissors_div.addEventListener( 'click', () => game("tesoura") );
-    restart_button.addEventListener( 'click', () => restart() );
+    restart_button.addEventListener( 'click', () => restartGame() );
 }
 
 main();
