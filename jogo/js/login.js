2f9261194f40914b2c62057e757650aaebf8f69a
MaiaLucas <barretomaia5466@outlook.com>
2019-02-16 02:09:11 -0300

    Create login.js

---
+++ jogo/js/login.js
@@ -0,0 +1,14 @@
+const username_input = document.getElementById("username");
+const login_button   = document.getElementById('login'); 
+
+function enterUsername (username) {
+    username = username_input.innerHTML;
+
+    localStorage.setItem('username', username_storage);
+}
+
+function main() {
+    login_button.addEventListener( 'click', () => enterUsername(username_input.innerHTML) );
+}
+
+main();
